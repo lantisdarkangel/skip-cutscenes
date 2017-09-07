b3957cb4b0bd0ad565770af5c6c7e5829ab8318b
lantisdarkangel <lantisdarkangel@hotmail.com>
2017-09-07 14:06:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
+const format = require('./format.js');
+
 module.exports = function SkipCutscenes(dispatch) {
-  dispatch.hook('sPlayMovie', (event) => {
-    dispatch.toServer('cEndMovie', Object.assign({ unk: true }, event));
-    return false;
-  });
-};
+	let enabled = false;
+	
+	const chatHook = event => {		
+		let command = format.stripTags(event.message).split(' ');
+		
+		if (['!sc', '!cgi'].includes(command[0].toLowerCase())) {
+			toggleModule();
+			return false;
+		}
+	}
+	dispatch.hook('C_CHAT', 1, chatHook)	
+	dispatch.hook('C_WHISPER', 1, chatHook)
+	
+	// slash support
+	try {
+		const Slash = require('slash')
+		const slash = new Slash(dispatch)
+		slash.on('sc', args => toggleModule())
+		slash.on('cgi', args => toggleModule())
+	} catch (e) {
+		// do nothing because slash is optional
+	}
+	
+	function toggleModule() {
+		enabled = !enabled;
+		systemMessage((enabled ? 'Cutscene Disabled' : 'Cutscene Enabled'));
+	}
+	
+	function systemMessage(msg) {
+	dispatch.toClient('S_CHAT', 1, {
+		channel: 24, //21 = p-notice, 24 = system
+		authorName: 'CGI',
+		message: msg
+	});
+}
+	if (!enabled) {
+	dispatch.hook('sPlayMovie', (event) => {
+	dispatch.toServer('cEndMovie', Object.assign({ unk: true }, event));
+	return false;
+	});
+}};
